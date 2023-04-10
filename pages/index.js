e003d1be5a1e3c4a78840314f450933b923e872a
Stefan <stefan.plopianu@gmail.com>
2023-04-10 11:01:57 +0300

    Finally a design i like

--- pages/index.js
+++ pages/index.js
@@ -43,43 +43,6 @@ export default function Home() {
             </div>
           </footer>
         </div>
-        <div className={styles.sidebar}>
-          <nav>
-            <ul>
-              <li>
-                <div class="home-icon">
-                  <div class="roof">
-                    <div class="roof-edge"></div>
-                  </div>
-                  <div class="front"></div>
-                </div>
-              </li>
-              <li>
-                <div class="about-icon">
-                  <div class="head">
-                    <div class="eyes"></div>
-                    <div class="beard"></div>
-                  </div>
-                </div>
-              </li>
-              <li>
-                <div class="work-icon">
-                  <div class="paper"></div>
-                  <div class="lines"></div>
-                  <div class="lines"></div>
-                  <div class="lines"></div>
-                </div>
-              </li>
-              <li>
-                <div class="mail-icon">
-                  <div class="mail-base">
-                    <div class="mail-top"></div>
-                  </div>
-                </div>
-              </li>
-            </ul>
-          </nav>
-        </div>
         <div className={styles.content}>
           <div className={styles.contentBox}>
             <div className={styles.contentDetails}>
